d40b06aad52caa248f3334f5a0f77394d5fb8d72
ELHARFALI <elharfali.youssef.20@gmail.com>
2022-10-09 15:49:36 +0100

    Main section for desktop

--- App/js/main.js
+++ App/js/main.js
@@ -2,6 +2,48 @@ const menu = document.querySelector('.menu');
 const close = document.querySelector('.close');
 const nav = document.querySelector('.nav');
 const overlay = document.querySelector('.overlay');
+const shopIcon = document.querySelector(".shop-icon");
+let num = document.querySelector('.num');
+const shoping = document.querySelector(".shoping");
+const mines = document.querySelector(".mines");
+const plus = document.querySelector(".plus");
+const btn = document.querySelector(".btn");
+let number = document.querySelector(".number p");
+
+
+let count = 0;
+
+plus.addEventListener('click', function () {
+    count++;
+    number.textContent = count;
+})
+
+mines.addEventListener("click", function () {
+
+    if (count === 0) {
+        count === 0;
+        number.textContent = count;
+    } else {
+        count--;
+        number.textContent = count;
+    }
+});
+
+btn.addEventListener('click', function () {
+    
+    if (count == 0) {
+        num.textContent = ''
+    } else {
+        num.classList.toggle("active");
+    }
+
+    num.textContent = count;
+})
+
+
+
+
+
 
 menu.addEventListener('click', function () {
     nav.classList.toggle('active')
@@ -17,3 +59,11 @@ overlay.addEventListener('click', function () {
     nav.classList.toggle("active");
     overlay.classList.toggle("overlay-active");
 })
+
+shopIcon.addEventListener('click', function () {
+    shoping.classList.toggle('is-active')
+})
+
+num.addEventListener("click", function () {
+    shoping.classList.toggle("is-active");
+});
